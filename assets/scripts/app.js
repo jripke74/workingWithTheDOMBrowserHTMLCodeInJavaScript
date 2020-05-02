5c0cecdc3e5013652456b6c19d1655e7dd15eab2
jripke74 <jripke74@gmail.com>
2020-05-02 06:54:56 -0700

    add closing modal and cancel controls

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,25 @@
-const addMovieModal = document.getElementById('add-modal');
-const startAddMovieButton = document.querySelector('header button');
-const backdrop = document.getElementById('backdrop');
+const addMovieModalEl = document.getElementById('add-modal');
+const startAddMovieButtonEl = document.querySelector('header button');
+const backdropEl = document.getElementById('backdrop');
+const cancelAddMovieButtonEl = addMovieModalEl.querySelector('.btn--passive');
 
 const toggleBackdrop = () => {
-  backdrop.classList.toggle('visible');
+  backdropEl.classList.toggle('visible');
 }
 
 const toggleMovieModal = () => {
-  addMovieModal.classList.toggle('visible');
+  addMovieModalEl.classList.toggle('visible');
   toggleBackdrop();
 };
 
-startAddMovieButton.addEventListener('click', toggleMovieModal);
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButtonEl.addEventListener('click', toggleMovieModal);
+backdropEl.addEventListener('click', backdropClickHandler);
+cancelAddMovieButtonEl.addEventListener('click', cancelAddMovie);
